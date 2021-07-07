9487527cd9d9f8eb85260610dc6e487e4f5dfd0e
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-07-07 13:44:05 -0400

    Working Code for Rock Paper Scissor game

---
+++ Stu_Mini-Project_03/script.js
@@ -0,0 +1,45 @@
+var wins =0,lost =0,tie =0;
+var computerPrompt =["R","S","P"];
+
+window.alert("Lets Play!!");
+
+      function playGame() {
+    let userInput = prompt("what is your choice");
+    if (!userInput){
+        return;
+    }
+
+    var userPrompt = userInput.toUpperCase();
+    var random = Math.floor(Math.random() * computerPrompt.length);
+    var computerSelection = prompt("Computer selects : ", computerPrompt[random]);
+    gameFunction (userPrompt,computerSelection);
+
+    if (window.confirm("Do you want to play more")) {
+        playGame();
+      } else {
+         window.alert("This is the Summary!!" + "\n" +"wins: " + wins + "\n" +"lost: " + lost + "\n" +"Tie: " + tie);
+      }   
+}
+
+function gameFunction (userPrompt,computerSelection) {
+     if (userPrompt===computerSelection)
+     {
+         tie= tie+1;
+         
+        window.alert("It is a Tie!! " + "\n" +"wins: " + wins + "\n" +"lost: " + lost + "\n" +"Tie: " + tie);
+     } else if ((userPrompt=="R" && computerSelection == "S") || (userPrompt=="S" && computerSelection == "P") || (userPrompt=="P" && computerSelection == "R")) {
+        wins=wins+1;
+        window.alert("You Win!!" + "\n" +"wins: " + wins + "\n" +"lost: " + lost + "\n" +"Tie: " + tie);
+        
+
+     } else {
+        lost++
+        window.alert("You Lost!!" + "\n" +"wins: " + wins + "\n" +"lost: " + lost + "\n" +"Tie: " + tie);
+       
+     }
+    return ;
+    
+ }
+ playGame();
+
+
